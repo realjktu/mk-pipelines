3694a10694692daa277b3bb7d48355327bf98501
Oleg Iurchenko <oiurchenko@mirantis.com>
2017-09-14 23:02:45 +0300

    fix

--- cleanup-stacks-pipeline.groovy
+++ cleanup-stacks-pipeline.groovy
@@ -11,11 +11,11 @@
  *   OPENSTACK_API_PROJECT_DOMAIN  OpenStack project domain
  *   OPENSTACK_API_PROJECT_ID      OpenStack project do id
  *   OPENSTACK_API_USER_DOMAIN     OpenStack user domain
- * 
- *   RETENTION_DAYS                Days to delete stacks after creation
- *   JOB_NAME                      Jenkins job name to inspect
  *
- * 
+ *   RETENTION_DAYS                Days to delete stacks after creation
+ *   JOBS_LIST                     Jenkins comma separated job names list to inspect outdated stacks
+ *
+ *
  */
 
 
@@ -34,30 +34,28 @@ node ('python') {
                 OPENSTACK_API_PROJECT_ID, OPENSTACK_API_USER_DOMAIN,
                 OPENSTACK_API_VERSION)
             openstack.getKeystoneToken(openstackCloud, venv)
-            String[] jobNames = JOB_NAME.split(',')
-            ArrayList<String> existingStacks=new ArrayList<String>()
-            
+            String[] jobNames = JOBS_lIST.tokenize(',')
+            ArrayList<String> existingStacks = []
             // Get list of stacks
-            for(def i=0;i<jobNames.size();i++){
-                existingStacks.addAll(openstack.getStacksForNameContains(openstackCloud, jobNames[i], venv))
+            for (def i=0; i < jobNames.size; i++){
+                existingStacks.addAll(openstack.getStacksForNameContains(openstackCloud, jobNames.get(i), venv))
             }
-            println "Found "+existingStacks.size()+" stacks"
+            println "Found " + existingStacks.size() + " stacks"
             // Check each stack
-            long currentTimestamp = (long) new Date().getTime()/1000;
+            long currentTimestamp = (long) new Date().getTime() / 1000;
             for(def i=0;i<existingStacks.size();i++){
                 def stackName = existingStacks.get(i)
                 def stackInfo = openstack.getHeatStackInfo(openstackCloud, stackName, venv)
                 //println stackInfo
-                println "Stack: "+stackName+" Creation time: "+ stackInfo.creation_time
-            
-                Date creationDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").parse(stackInfo.creation_time.trim())
+                println "Stack: " + stackName + " Creation time: " + stackInfo.creation_time
+                Date creationDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH).parse(stackInfo.creation_time.trim())
 		//Date creationDate = new Date().parse("yyyy-MM-dd'T'HH:mm:ss'Z'", stackInfo.creation_time)
-                long creationTimestamp = (long) creationDate.getTime()/1000
+                long creationTimestamp = (long) creationDate.getTime() / 1000
                 def diff = currentTimestamp-creationTimestamp
-                def retentionSec = 	Integer.parseInt(RETENTION_DAYS)*86400
+                def retentionSec = Integer.parseInt(RETENTION_DAYS) * 86400
                 if (diff > retentionSec){
-                    println stackName+" stack have to be deleted"
-                    //openstack.deleteHeatStack(openstackCloud, stackName, venv)hhhhh
+                    println stackName + " stack have to be deleted"
+                    //ooopenstack.deleteHeatStack(openstackCloud, stackName, venv)
                 }
             }
         }
