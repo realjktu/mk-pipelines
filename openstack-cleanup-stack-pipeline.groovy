8a7464b2f775aa345655f1affd9d82048c387722
Oleg Iurchenko <oiurchenko@mirantis.com>
2017-09-14 18:05:55 +0300

    fix for openstack-cleanup-stack-pipeline

--- openstack-cleanup-stack-pipeline.groovy
+++ openstack-cleanup-stack-pipeline.groovy
@@ -22,7 +22,7 @@
 //java.util.Date t1=new java.util.Date()
 //import java.util.Date
 t1=new Date()
-t1.parse('1973/07/21')
+t1.parse('2017-08-30T09:05:08Z')
 
 
 openstack = new com.mirantis.mk.Openstack()
