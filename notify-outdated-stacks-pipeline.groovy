e5ca14a285108d6344c36e39b6e3cf092fcd7206
Oleg Iurchenko <oiurchenko@mirantis.com>
2017-10-30 13:12:27 +0200

    test01

--- notify-outdated-stacks-pipeline.groovy
+++ notify-outdated-stacks-pipeline.groovy
@@ -25,7 +25,8 @@ import java.text.SimpleDateFormat
 
 node ('python') {
     try {
-        sh 'curl -X POST -H \'Content-type: application/json\' --data \'{"text":"Hello, World!"}\' https://hooks.slack.com/services/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'
+        def text = "Some new test"
+        sh 'curl -X POST -H \'Content-type: application/json\' --data \'{"text":"'+text+'"}\' https://hooks.slack.com/services/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'
 
         HashMap<String, String> outdatedStacks = new HashMap<String, String>()
         stage('Looking for outdated stacks') {
